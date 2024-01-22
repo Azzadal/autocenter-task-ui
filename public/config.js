5b93bcb22ebb15463dd6f2d4d5cfc7fcdbb6a6ee
Azzadal <kvasov2803@gmail.com>
2024-01-22 16:26:02 +0700

    server config change

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 
 // IP-адрес или DNS-имя API сервера
-var API_SERVER_HOST = 'localhost';
+var API_SERVER_HOST = 'autocenter-whqjyruf.b4a.run';
 
 // Порт API сервера
-var API_SERVER_PORT = 8080;
+var API_SERVER_PORT = 443;
 
 // Использовать SSL при обращению к серверу API
 var API_USING_SSL = true;
